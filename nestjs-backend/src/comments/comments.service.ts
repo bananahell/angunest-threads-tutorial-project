223f4b1de4eb19ae87f7d35a062154e8a333c3ec
Pedro Nogueira <cmcg89034@gmail.com>
2024-03-09 08:52:10 -0300

    🗃️ Create comment route created!

--- nestjs-backend/src/comments/comments.service.ts
+++ nestjs-backend/src/comments/comments.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Comment } from './schemas/comment.schema';
 
 @Injectable()
 export class CommentsService {
+  constructor(@InjectModel(Comment.name) private commentModel: Model<Comment>) {}
+
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    const newComment = this.commentModel.create({
+      content: createCommentDto.content,
+      parent: createCommentDto.parentId || null,
+      user: createCommentDto.userId,
+    });
+    return newComment.then((doc) => {
+      return doc.populate(['user', 'parent']);
+    });
   }
 
   findAll() {
-    return `This action returns all comments`;
+    return this.commentModel.find().populate(['user', 'parent']).exec();
   }
 
   findOne(id: number) {
