cac7b6eb56131b2a92a51b66cbfe350c4356c3e0
Pedro Nogueira <cmcg89034@gmail.com>
2024-03-09 07:35:42 -0300

    🗃️ Added database and variable security to project!

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, CommentsModule],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    // async injection in MongooseModule to access .env config in imports
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
