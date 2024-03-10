3662a4087e34933b9164c2203079691884acce3b
Pedro Nogueira <cmcg89034@gmail.com>
2024-03-10 08:06:27 -0300

    ✨ Established API communication - fetches top levels comments!

--- angular-frontend/src/app/components/comment/comment.component.ts
+++ angular-frontend/src/app/components/comment/comment.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CommentFormComponent],
   templateUrl: './comment.component.html',
-  styleUrl: './comment.component.scss'
+  styleUrl: './comment.component.scss',
 })
 export class CommentComponent {
+  isExpanded = false;
+  isReplying = false;
 
+  toggleReplying() {
+    this.isReplying = !this.isReplying;
+    if (this.isReplying) {
+      this.isExpanded = true;
+    }
+  }
+
+  toggleExpanded() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
---
+++ angular-frontend/src/app/services/comment.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommentService {
+  http = inject(HttpClient);
+
+  getComments(parentId: string = '') {
+    let url = `${environment.apiBaseUrl}/comments`;
+    if (parentId) {
+      url += `?parentId=${parentId}`;
+    }
+    return this.http.get<Comment[]>(url);
+  }
+}
