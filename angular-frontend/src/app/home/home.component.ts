a7cf52425d80d145670d0d7c8898e0862241b916
Pedro Nogueira <cmcg89034@gmail.com>
2024-03-10 06:55:02 -0300

    💄 Created routing for home and comments page!

---
+++ angular-frontend/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommentComponent } from '../components/comment/comment.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommentComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
